cbb48a1a2f9698ffad29eb8437e86565f851c39b
Salil Vartak <salilv1103@gmail.com>
2026-03-08 20:16:09 +0530

    feat: Initialize Flutter project structure and integrate Firebase services.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,8 +5,17 @@ plugins {
     id("com.google.gms.google-services") 
 }
 
+import java.io.FileInputStream
+import java.util.Properties
+
+val keystorePropertiesFile = rootProject.file("key.properties")
+val keystoreProperties = Properties()
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 android {
-    namespace = "com.example.chore"
+    namespace = "com.salilvartak.huddleup"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -18,11 +27,20 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = "17"
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+            storeFile = keystoreProperties["storeFile"]?.let { file(it as String) }
+            storePassword = keystoreProperties["storePassword"] as String?
+        }
     }
 
     defaultConfig {
-        applicationId = "com.example.chore"
+        applicationId = "com.salilvartak.huddleup"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -31,7 +49,7 @@ android {
 
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
--- android/app/src/main/kotlin/com/salilvartak/huddleup/MainActivity.kt
+++ android/app/src/main/kotlin/com/salilvartak/huddleup/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.chore
+package com.salilvartak.huddleup
 
 import io.flutter.embedding.android.FlutterActivity
 
